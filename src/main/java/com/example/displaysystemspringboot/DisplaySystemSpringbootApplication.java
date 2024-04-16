cac2215c3feff0b2771801dd46cd0e319f4da6cd
filip <filipkleimert@gmail.com>
2024-04-16 11:27:02 +0200

    First

---
+++ src/main/java/com/example/displaysystemspringboot/DisplaySystemSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.displaysystemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplaySystemSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisplaySystemSpringbootApplication.class, args);
+    }
+
+}
