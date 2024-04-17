8508bb370d41b0c95e471a3d79947e00992d02d1
RomFur <roman-1996@hotmail.com>
2024-04-17 20:22:43 +0200

    Printing room object to web page

--- src/main/java/com/example/displaysystemspringboot/model/Activity.java
+++ src/main/java/com/example/displaysystemspringboot/model/Activity.java
@@ -1,4 +1,57 @@
 package com.example.displaysystemspringboot.model;
 
+import java.time.LocalDateTime;
+
 public class Activity {
+
+    private String actName;
+    private LocalDateTime startTime, endTime;
+    private String user;
+
+    public Activity(LocalDateTime startTime, LocalDateTime endTime, String user) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.user = user;
+    }
+
+    public Activity(String actName, LocalDateTime startTime, LocalDateTime endTime, String user) {
+        this.actName = actName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.user = user;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Activity{" +
+                "actName='" + actName + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", user='" + user + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/displaysystemspringboot/model/Room.java
+++ src/main/java/com/example/displaysystemspringboot/model/Room.java
@@ -1,4 +1,37 @@
 package com.example.displaysystemspringboot.model;
 
+import java.util.List;
+
 public class Room {
+
+    private String roomId;
+    private List<Activity> activities;
+
+    public Room(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<Activity> activities) {
+        this.activities = activities;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomId='" + roomId + '\'' +
+                ", activities=" + activities +
+                '}';
+    }
 }
